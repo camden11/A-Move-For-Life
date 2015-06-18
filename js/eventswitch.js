7210d0a218c028e592d51caa382c90cb335e7e47
camden11 <camden2112@gmail.com>
2015-06-17 22:35:07 -0400

    reorganized directory

---
+++ js/eventswitch.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+
+	var upcoming = true;
+
+	$('#upcoming').click(function() {
+		if (!upcoming) {
+			upcoming = true;
+			$(this).toggleClass("active-button");
+			$(this).toggleClass("inactive-button");
+			$('#past').toggleClass("active-button");
+			$('#past').toggleClass("inactive-button");
+			$('#past-event-wrapper').toggleClass('fadein');
+			$('#past-event-wrapper').toggleClass('fadeout');
+			$('#past-event-wrapper').css("visibility", "hidden");
+			$('#past-event-wrapper').css("opacity", "0");
+		}
+	});
+
+	$('#past').click(function() {
+		if (upcoming) {
+			upcoming = false;
+			$(this).toggleClass("active-button");
+			$(this).toggleClass("inactive-button");
+			$('#upcoming').toggleClass("active-button");
+			$('#upcoming').toggleClass("inactive-button");
+			$('#past-event-wrapper').toggleClass('fadein');
+			$('#past-event-wrapper').toggleClass('fadeout');
+			$('#past-event-wrapper').css("visibility", "visible");
+			$('#past-event-wrapper').css("opacity", "1");
+		}
+	});
+});
---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	$('#slideout').click(function() {
+		$('.slideout-menu').animate({left: '0%'}, 'fast');
+		$('.back').animate({left: '50%'}, 'fast');
+		$('body').animate({marginLeft: '50%'}, 'fast');
+	});
+
+
+	$('.back').click(function() {
+		$('.slideout-menu').animate({left: '-50%'}, 'fast');
+		$('.back').animate({left: '100%'}, 'fast');
+		$('body').animate({marginLeft: '0%'}, 'fast');
+	});
+});
