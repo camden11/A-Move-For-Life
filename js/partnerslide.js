4490eb2c6283130a96d9b47ccc276c41be407ee8
camden11 <camden2112@gmail.com>
2015-07-03 15:23:02 -0400

    partners slider arrow fadeout/in

--- js/partnerslide.js
+++ js/partnerslide.js
@@ -9,6 +9,12 @@ $(document).ready(function() {
 	var artistPos = 0;
 
 	$('.artist-left').click(function() {
+		if (artistPos == 1) {
+			$('.artist-left').css({"opacity": "0", "cursor": "auto"});
+		}
+		if (artistPos == artistLimit) {
+			$('.artist-right').css({"opacity": "1", "cursor": "pointer"});
+		}
 		if (artistPos > 0) {
 			artistPos = artistPos - 1;
 			$('.artists').animate({left: convertPos(artistPos)});
@@ -16,6 +22,12 @@ $(document).ready(function() {
 	});
 
 	$('.artist-right').click(function() {
+		if (artistPos == 0) {
+			$('.artist-left').css({"opacity": "1", "cursor": "pointer"});
+		}
+		if (artistPos == artistLimit - 1) {
+			$('.artist-right').css({"opacity": "0", "cursor": "auto"});
+		}
 		if (artistPos < artistLimit) {
 			artistPos = artistPos + 1;
 			$('.artists').animate({left: convertPos(artistPos)});
@@ -26,6 +38,12 @@ $(document).ready(function() {
 	var sponsorPos = 0;
 
 	$('.sponsor-left').click(function() {
+		if (sponsorPos == 1) {
+			$('.sponsor-left').css({"opacity": "0", "cursor": "auto"});
+		}
+		if (sponsorPos == sponsorLimit) {
+			$('.sponsor-right').css({"opacity": "1", "cursor": "pointer"});
+		}
 		if (sponsorPos > 0) {
 			sponsorPos = sponsorPos - 1;
 			$('.sponsors').animate({left: convertPos(sponsorPos)});
@@ -33,6 +51,12 @@ $(document).ready(function() {
 	});
 
 	$('.sponsor-right').click(function() {
+		if (sponsorPos == 0) {
+			$('.sponsor-left').css({"opacity": "1", "cursor": "pointer"});
+		}
+		if (sponsorPos == sponsorLimit - 1) {
+			$('.sponsor-right').css({"opacity": "0", "cursor": "auto"});
+		}
 		if (sponsorPos < sponsorLimit) {
 			sponsorPos = sponsorPos + 1;
 			$('.sponsors').animate({left: convertPos(sponsorPos)});
@@ -43,6 +67,12 @@ $(document).ready(function() {
 	var charityPos = 0;
 
 	$('.charity-left').click(function() {
+		if (charityPos == 1) {
+			$('.charity-left').css({"opacity": "0", "cursor": "auto"});
+		}
+		if (charityPos == charityLimit) {
+			$('.charity-right').css({"opacity": "1", "cursor": "pointer"});
+		}
 		if (charityPos > 0) {
 			charityPos = charityPos - 1;
 			$('.charities').animate({left: convertPos(charityPos)});
@@ -50,6 +80,12 @@ $(document).ready(function() {
 	});
 
 	$('.charity-right').click(function() {
+		if (charityPos == 0) {
+			$('.charity-left').css({"opacity": "1", "cursor": "pointer"});
+		}
+		if (charityPos == charityLimit - 1) {
+			$('.charity-right').css({"opacity": "0", "cursor": "auto"});
+		}
 		if (charityPos < charityLimit) {
 			charityPos = charityPos + 1;
 			$('.charities').animate({left: convertPos(charityPos)});
