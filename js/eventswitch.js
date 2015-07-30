977f152e73da39f79abcb0f5ac1c5cf6c2e361d5
camden11 <camden2112@gmail.com>
2015-07-30 17:13:10 -0400

    minor layout adits

--- js/eventswitch.js
+++ js/eventswitch.js
@@ -13,10 +13,12 @@ $(document).ready(function() {
 			$('#upcoming-event-wrapper').toggleClass('fadeout');
 			$('#upcoming-event-wrapper').css('position', 'relative');
 			$('#upcoming-event-wrapper').css('opacity', '1');
+			$('#upcoming-event-wrapper').css('z-index', '3');
 			$('#event-footer').css('display', 'block')
 			$('#past-event-wrapper').toggleClass('fadein');
 			$('#past-event-wrapper').toggleClass('fadeout');
-			$('#past-event-wrapper').css('opacity', '0')
+			$('#past-event-wrapper').css('opacity', '0');
+			$('#past-event-wrapper').css('z-index', '0');
 			$('#past-event-wrapper').css('position', 'fixed');
 		}
 	});
@@ -32,11 +34,13 @@ $(document).ready(function() {
 			$('#past-event-wrapper').toggleClass('fadeout');
 			$('#past-event-wrapper').css('position', 'relative');
 			$('#past-event-wrapper').css('opacity', '1');
+			$('#past-event-wrapper').css('z-index', '3');
 			$('#event-footer').css('display', 'none')
 			$('#upcoming-event-wrapper').toggleClass('fadein');
 			$('#upcoming-event-wrapper').toggleClass('fadeout');
 			$('#upcoming-event-wrapper').css('position', 'fixed');
 			$('#upcoming-event-wrapper').css('opacity', '0');
+			$('#upcoming-event-wrapper').css('z-index', '0');
 		}
 	});
 });
